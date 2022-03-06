452516363e2b33172f29ce3011bb632ce4383d70
Pedro Longue <pedrolongue13@gmail.com>
2022-03-06 20:43:08 -0300

    Adiciona o newsLetter mboile e desktop

--- desafio-react/src/pages/Home.tsx
+++ desafio-react/src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Header } from "../components/Header/HeaderDesktop";
+import { NewsLetter } from "../components/NewsLetter/NewsLetter";
 
 function App() {
   return (
     <div>
       <Header />
+      <NewsLetter />
     </div>
   );
 }
