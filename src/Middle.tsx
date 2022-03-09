7512a95bb86bba006ebc9a873e0d417e690f654e
Pedro Aldo <pedroaldo2020@gmail.com>
2022-03-09 15:24:04 -0300

    Pequenos reparos no header e ajusta o formulario desktop

--- src/Middle.tsx
+++ src/Middle.tsx
@@ -30,6 +30,7 @@ const Middle = () => {
         setContato(false);
         setFormulario(false);
         forma ? setForma (false) : setForma(true);
+        forma ? setSobre(false) : setSobre (true);
     }
 
     const entregaActive = () => {
@@ -39,17 +40,20 @@ const Middle = () => {
         setSeguranca(false);
         setContato(false);
         setFormulario(false);
+        entrega ? setSobre(false) : setSobre (true);
         entrega ? setEntrega (false) : setEntrega(true);
+
     }
 
     const trocaActive = () => {
-        setSobre(false);
+        setSobre(true);
         setForma(false);
         setEntrega(false);
         setSeguranca(false);
         setContato(false);
         setFormulario(false);
         troca ? setTroca (false) : setTroca(true);
+        troca ? setSobre(false) : setSobre (true);
     }
 
     const segurancaActive = () => {
@@ -60,6 +64,7 @@ const Middle = () => {
         setContato(false);
         setFormulario(false);
         seguranca ? setSeguranca (false) : setSeguranca(true);
+        seguranca ? setSobre(false) : setSobre (true);
     }
 
     const contatoActive = () => {
@@ -68,29 +73,21 @@ const Middle = () => {
         setEntrega(false);
         setTroca(false);
         setSeguranca(false);
+        contato ? setSobre(false) : setSobre (true);
         contato ? setFormulario(false) : setFormulario(true);
         contato ? setContato (false) : setContato(true);
-    }
-    const formActive = () => {
-        setSobre(false);
-        setForma(false);
-        setEntrega(false);
-        setTroca(false);
-        setSeguranca(false);
-        setContato(true);
-        formulario ? setFormulario (true) : setContato(true);
-    }
+}
     return (
         <section className="middle-main">
             <div className="middle-icons">
                 <img className="middle-icons-home" src={HomeIcon} alt="Home icon" />
                 <img src={ArrowRight} alt="Arrow icon" />
-                <p onClick={contatoActive} className={contato ? "middle-title-form" : "middle-title"}>INSTITUCIONAL</p>
+                <p className="middle-icons-title">INSTITUCIONAL</p>
             </div>
             <h1 className="middle-title">INSTITUCIONAL</h1>
             <div className="middle-description">
-                <div className="middle-description-infos">
-                    <button onClick={sobreActive} className= {sobre ? "middle-contact-active" : "middle-contact-disabled"}>Sobre</button>
+                <div className={contato ? "middle-description-infos-contact": "middle-description-infos"}>
+                    <button onClick={sobreActive} className= {sobre ? "middle-contact-disabled" : "middle-contact-active"}>Sobre</button>
                     <button onClick={formaActive} className= {forma ? "middle-contact-active" : "middle-contact-disabled"}>Forma de Pagamento</button>
                     <button onClick={entregaActive} className= {entrega ? "middle-contact-active" : "middle-contact-disabled"}>Entrega</button>
                     <button onClick={trocaActive} className= {troca ? "middle-contact-active" : "middle-contact-disabled"}>Troca e Devolução</button>
@@ -98,8 +95,8 @@ const Middle = () => {
                     <button onClick={contatoActive} className= {contato ? "middle-contact-active-form" : "middle-contact-disabled"}>Contato</button>
                 </div>
                 <div className="middle-description-content">
-                    <h1 onClick={contatoActive} className={contato ? "middle-description-content-title-none" : "middle-description-content-title"}>Sobre</h1>
-                        <p onClick={contatoActive} className={contato ? "middle-description-content-text-none" : "middle-description-content-text"}>
+                    <h1 className={contato ? "middle-description-content-none" : "middle-description-content-title"}>Sobre</h1>
+                        <p className={contato ?  "middle-description-content-none" : "middle-description-content-text"}>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
@@ -121,27 +118,40 @@ const Middle = () => {
                        <br /><br />
                         </p>
                 </div>
-                <section onClick={formActive} className={formulario ? "middle-form-active" : "middle-form-disabled"}>
-                <div className="middle-form">
-                    <h1 className="middle-form-title">PREENCHA O FORMULÁRIO</h1>
-                    <label className="middle-form-input-title">Nome</label>
-                    <input className="middle-form-input" type="text" placeholder="Seu nome completo" />
-                    <label className="middle-form-input-title" placeholder="Seu e-mail">E-mail</label>
-                    <input className="middle-form-input" type="text" placeholder="Seu e-mail"/>
-                    <label className="middle-form-input-title">CPF</label>
-                    <input className="middle-form-input" type="text" placeholder="000 000 000 00"/>
-                    <label className="middle-form-input-title">Data de Nascimento</label>
-                    <input className="middle-form-input" type="text" placeholder="00.00.0000"/>
-                    <label className="middle-form-input-title">Telefone</label>
-                    <input className="middle-form-input" type="text" placeholder="(+00) 00000 0000"/>
-                    <label className="middle-form-input-title">Instagram</label>
-                    <input className="middle-form-input" type="text" placeholder="@seuuser"/>
-                    <label >*Declaro que li e aceito</label>
-                    <input type="radio" />
-                    <input type="submit" value="CADASTRE-SE" />
-                </div>
+                <div className={formulario ? "middle-form-active" : "middle-form-disabled"}>
+                    <div className="middle-form">
+                        <h1 className="middle-form-title">PREENCHA O FORMULÁRIO</h1>
+                        <div className="middle-form-labels">
+                            <label className="middle-form-input-title">Nome</label>
+                        <input className="middle-form-input" type="text" placeholder="Seu nome completo" />
 
-            </section>
+                        <label className="middle-form-input-title" placeholder="Seu e-mail">E-mail</label>
+                        <input className="middle-form-input" type="text" placeholder="Seu e-mail"/>
+
+                        <label className="middle-form-input-title">CPF</label>
+                        <input className="middle-form-input" type="text" placeholder="000 000 000 00"/>
+
+                        <label className="middle-form-input-title">Data de Nascimento</label>
+                        <input className="middle-form-input" type="text" placeholder="00.00.0000"/>
+
+                        <label className="middle-form-input-title">Telefone</label>
+                        <input className="middle-form-input" type="text" placeholder="(+00) 00000 0000"/>
+
+                        <label className="middle-form-input-title">Instagram</label>
+                        <input className="middle-form-input" type="text" placeholder="@seuuser"/>
+
+                        <div className="middle-form-terms">
+                            <a href="/">
+                                <label className="middle-form-asterisk">*</label><label className="middle-form-terms-text">Declaro que li e aceito</label>
+                            </a>
+                            <input className="middle-form-terms-check" type="checkbox" />
+                        </div>
+
+                        <input className="middle-form-submit" type="submit" value="CADASTRE-SE" />
+                        </div>
+
+                    </div>
+                </div>
             </div>
         </section>
     )
