af2e533c95c9eb82bb911db35f1a57f0199c8a65
Pedro Aldo <pedroaldo2020@gmail.com>
2022-03-14 13:57:46 -0300

    Cria as opções do header mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 
 import "./Header.css";
 import LogoM3 from "../images/m3-logo.png";
 import Cart from "../images/cart-icon.png";
 import Search from "../images/search-icon.png"
-import MenuIcon from "../images/menu-icon-mobile.png";
 
 const Header = () => {
+    const [openMenu, setOpenMenu] = useState (false);
+
+    const clickOpenMenu = () => {
+       openMenu ? setOpenMenu(false) : setOpenMenu(true);
+   }
+
     return (
     <section>
         <div className="header-desktop-wrapper">
@@ -44,9 +49,20 @@ const Header = () => {
                     <h2 className="sub-header-desktop-info-text" >SAIBA MAIS</h2>
                 </a>
             </div>
-           <div className="header-mobile-wrapper">
+           <div className={openMenu ? "header-mobile-wrapper-menu-active" : "header-mobile-wrapper"}>
                 <div className="header-mobile-title">
-                    <a href="/">    <img className="header-mobile-menu" src={MenuIcon} alt="Menu Icon" />  </a>
+                    <button onClick={clickOpenMenu} className="header-mobile-menu">
+                            <div className={openMenu ? "menu-icon-open menu-icon-open-line1" : "menu-icon-closed"}></div>
+                            <div className={openMenu ? "menu-icon-open menu-icon-open-line1" : "menu-icon-closed"}></div>
+                            <div className={openMenu ? "menu-icon-open menu-icon-open-line2" : "menu-icon-closed"}></div>
+                    </button>
+                    <div className={openMenu ? "background-active" : "background-disabled"}>
+                        <div className={openMenu ? "menu-active" : "menu-disabled"}>
+                            <h5 className="menu-titles menu-titles-line1" >Entrar</h5>
+                            <h5 className="menu-titles menu-titles-line2" >Cursos</h5>
+                            <h5 className="menu-titles" >Saiba Mais</h5>
+                        </div>
+                    </div>
                     <div className="header-mobile-logo">
                     <a href="/">   <img src={LogoM3} alt="Logo M3" />  </a>
                     <a href="/">    <h1 className="header-mobile-wrapper-logo-title">academy</h1></a>
